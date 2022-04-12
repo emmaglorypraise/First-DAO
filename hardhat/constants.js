074f7f63406aa3cb10111972440bb49501a37a43
emmaglorypraise <emmaglorypraise@gmail.com>
2022-04-12 17:17:49 +0100

    added dao-app

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x1C42c0cb177B8CBb0A6c79CbF543910455bca2BA";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
